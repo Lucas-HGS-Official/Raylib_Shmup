0108bbe230092c0b212ce8cedbbce694de02d0bd
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2024-11-06 20:32:49 -0300

    made the player move with keyboar inputs

--- Code/game.c
+++ Code/game.c
@@ -14,8 +14,8 @@ void main() {
     Vector2 playerPos = {(screenWidth/2)-playerTexture.width/2, ((screenHeight/4)*3)-playerTexture.height/2};
     Rectangle playerSrcRect = {0,0, playerTexture.width, playerTexture.height};
     Rectangle playerDestRect = {playerPos.x, playerPos.y,  playerTexture.width, playerTexture.height};
-    // Vector2 playerDirectionVec = { 2, -2 };
-    float playerSpeed = 100.f;
+    Vector2 playerDirectionVec = { 0, 0 };
+    float playerSpeed = 500.f;
 
     Texture2D starTexture = LoadTexture("../Assets/images/star.png");
     SetRandomSeed((unsigned int)time(NULL));
@@ -31,10 +31,16 @@ void main() {
     // SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        
+        playerDirectionVec.x = IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT);
+        playerDirectionVec.y = IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP);
+        playerDirectionVec = Vector2Normalize(playerDirectionVec);
 
-        // playerPos = Vector2Add(playerPos, Vector2Scale(playerDirectionVec, playerSpeed * GetFrameTime()));
-        playerDestRect.x = GetMousePosition().x;
-        playerDestRect.y = GetMousePosition().y;
+        playerPos = Vector2Add(playerPos, Vector2Scale(playerDirectionVec, playerSpeed * GetFrameTime()));
+        playerDestRect.x = playerPos.x;
+        playerDestRect.y = playerPos.y;
+
+        
         BeginDrawing();
 
             ClearBackground(DARKBLUE);
