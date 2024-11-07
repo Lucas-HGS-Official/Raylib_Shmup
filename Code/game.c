a607eef85131f10fc1c1e32f4b6fa88fe25346e1
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2024-11-07 18:25:40 -0300

    Reorganizing code, separating the player code

--- Code/game.c
+++ Code/game.c
@@ -3,6 +3,18 @@
 #include <time.h>
 #include <stdio.h>
 
+struct Sprite {
+    Texture2D spriteTexture;
+    Rectangle SrcRect;
+    Rectangle DestRect;
+    Vector2 origin;
+}typedef Sprite;
+
+
+Sprite PlayerInit();
+
+Vector2 PlayerControls(Vector2 playerDirectionVec, Vector2 playerPos, float playerSpeed, Sprite* player);
+
 
 void main() {
     const int screenWidth = 1366;
@@ -10,10 +22,9 @@ void main() {
 
     InitWindow(screenWidth, screenHeight, "Ray Space Shooter");
 
-    Texture2D playerTexture = LoadTexture("../Assets/images/player.png");
-    Vector2 playerPos = {(screenWidth/2)-playerTexture.width/2, ((screenHeight/4)*3)-playerTexture.height/2};
-    Rectangle playerSrcRect = {0,0, playerTexture.width, playerTexture.height};
-    Rectangle playerDestRect = {playerPos.x, playerPos.y,  playerTexture.width, playerTexture.height};
+    Sprite player = PlayerInit();
+    Vector2 playerPos = {(screenWidth/2)-player.spriteTexture.width/2, ((screenHeight/4)*3)-player.spriteTexture.height/2};
+    player.DestRect.x = playerPos.x; player.DestRect.y = playerPos.y; 
     Vector2 playerDirectionVec = { 0, 0 };
     float playerSpeed = 500.f;
 
@@ -31,14 +42,8 @@ void main() {
     // SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        
-        playerDirectionVec.x = IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT);
-        playerDirectionVec.y = IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP);
-        playerDirectionVec = Vector2Normalize(playerDirectionVec);
 
-        playerPos = Vector2Add(playerPos, Vector2Scale(playerDirectionVec, playerSpeed * GetFrameTime()));
-        playerDestRect.x = playerPos.x;
-        playerDestRect.y = playerPos.y;
+        playerPos = PlayerControls(playerDirectionVec, playerPos, playerSpeed, &player);
 
         
         BeginDrawing();
@@ -49,14 +54,45 @@ void main() {
                 DrawTexture(starTexture, starListPosX[i], starListPosY[i], WHITE);
             }
 
-            DrawTexturePro(playerTexture, playerSrcRect, playerDestRect, (Vector2){ playerTexture.width/2, playerTexture.height/2 }, 0.f, WHITE);
+            DrawTexturePro(player.spriteTexture, player.SrcRect, player.DestRect, player.origin, 0.f, WHITE);
 
 
         EndDrawing();
     }
 
-    UnloadTexture(playerTexture);
+    UnloadTexture(player.spriteTexture);
     UnloadTexture(starTexture);
 
     CloseWindow();
+}
+
+
+
+Sprite PlayerInit() {
+    Sprite playerSprite;
+
+    playerSprite.spriteTexture = LoadTexture("../Assets/images/player.png");
+
+    Rectangle srcRect = {0, 0, playerSprite.spriteTexture.width, playerSprite.spriteTexture.height};
+    Rectangle destRect = {0, 0,  playerSprite.spriteTexture.width, playerSprite.spriteTexture.height};
+    Vector2 origin = {playerSprite.spriteTexture.width/2, playerSprite.spriteTexture.height/2};
+
+    playerSprite.DestRect = destRect;
+    playerSprite.SrcRect = srcRect;
+    playerSprite.origin = origin;
+
+    return playerSprite;
+}
+
+
+Vector2 PlayerControls(Vector2 playerDirectionVec, Vector2 playerPos, float playerSpeed, Sprite* player) {
+    playerDirectionVec.x = IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT);
+    playerDirectionVec.y = IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP);
+    playerDirectionVec = Vector2Normalize(playerDirectionVec);
+
+    playerPos = Vector2Add(playerPos, Vector2Scale(playerDirectionVec, playerSpeed * GetFrameTime()));
+    player->DestRect.x = playerPos.x;
+    player->DestRect.y = playerPos.y;
+
+    return playerPos;
 }
